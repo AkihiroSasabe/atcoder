55a4883b48bb3d9183c256fd9bde1443020d7f56
AkihiroSasabe <akki.edelweiss@gmail.com>
2022-10-17 01:39:55 +0900

    Add abc250 c

--- src/abc250/c.rs
+++ src/abc250/c.rs
@@ -5,60 +5,41 @@ fn main() {
     input! {
         n: usize,
         q: usize,
-        x: [usize; q]
+        mut x: [usize; q]
+    }
+    // 記載番号とindexを対応させる(1始まりではなく、0始まりにする)
+    for i in 0..x.len() {
+        x[i] -= 1;
     }
 
-    // i番目の要素に、i番目の位置にあるボールに "書かれている整数" が格納されている
-    let mut writen: Vec<usize> = (0..n).collect();
-
-    // i番目の要素に、整数iが書かれたボールの "位置" と、"整数i"が格納されている
-    let mut positions = vec![vec![0;2];n];
+    // 数列b: [key: 番号, value: 位置]
+    let mut b = vec![];
     for i in 0..n {
-        positions[i] = vec![i,i];
+        b.push(i);
     }
 
-    for i in x {
-        if positions[i-1][0] != n-1 {
-            // 今いる場所
-            let left_pos = positions[i-1][0].clone();
-            let right_val = writen[left_pos+1].clone();
-            let right_pos = positions[right_val][0].clone();
-            let left_val = i-1;
+    // 数列a: [key: 位置, value: 番号]
+    let mut a = b.clone();
 
-            positions[right_val][0] = left_pos;
-            positions[left_val][0] = right_pos;
-            writen[left_pos] = right_val;
-            writen[right_pos] = i-1;
+    for i in 0..x.len() {
+        let position = b[x[i]];
+        // クエリの位置は既に右端か?
+        if position != n - 1 {
+            let neighbor = a[position + 1];
+            // 右隣とswap
+            a.swap(position, position + 1); 
+            b.swap(neighbor, x[i]);
         }
         else {
-            // 今いる場所
-            // println!("owa {}", i);
-            let right_pos = positions[i-1][0].clone();
-            // println!("right_pos {}", right_pos);
-            let right_val = writen[i-1].clone();
-            let left_pos = right_pos - 1;
-            let left_val = writen[left_pos].clone();
-
-            positions[right_val][0] = left_pos;
-            positions[left_val][0] = right_pos;
-            writen[left_pos] = right_val;
-            writen[right_pos] = i-1;
+            // 左隣とswap
+            let neighbor = a[position - 1];
+            a.swap(position, position - 1);
+            b.swap(neighbor, x[i]);
         }
-        encode(&positions);
     }
 
-    // println!("positions: {:?}", positions);
-    positions.sort();
-    // println!("positions: {:?}", positions);
-
-    for i in positions {
-        print!("{} ", i[1] + 1);
+    for i in 0..n {
+        print!("{} ", a[i] + 1);
     }
-}
 
-// 
-fn encode(list: &Vec<Vec<usize>>) {
-    let mut list2 = list.clone();
-    list2.sort();
-    // println!("{:?}", list2);
 }

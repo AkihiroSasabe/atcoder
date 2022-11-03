996d1b531634db97b139541150f134d7d211c01b
AkihiroSasabe <akki.edelweiss@gmail.com>
2022-11-03 13:48:49 +0900

    Add abc275 e

--- src/abc275/e.rs
+++ src/abc275/e.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code, unused_imports)]
-use proconio::input;
 use itertools::Itertools;
-use std::cmp::{max, min};
+use proconio::input;
+use proconio::marker::Chars;
 use std::cmp::Ordering;
-use std::collections::VecDeque;
+use std::cmp::{max, min};
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use proconio::marker::Chars;
+use std::collections::VecDeque;
 use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
@@ -19,24 +19,54 @@ fn main() {
     }
 
     let modulo = 998244353;
-        let m_rev = mod_pow(m, modulo - 2, modulo);
+    let m_rev = mod_pow(m, modulo - 2, modulo);
 
-    let mut dp = vec![vec![0; n+1]; k];
-    for i in 1..(m+1) {
+    // println!("m_rev: {}", m_rev);
+
+    // dp[ルーレット回転数kk][マスi]: kk回ルーレットを回したときに、マスiに居る確率
+    let mut dp = vec![vec![0; n + 1]; k];
+    for i in 1..(m + 1) {
         dp[0][i] += m_rev;
     }
+    // println!("{:?}", dp[0]);
+
     for kk in 1..k {
-        for i in 1..(m+1) {
-            for j in 1..(m+1) {
-                dp[kk][i] = dp[kk-1][i-j] * 
+        for i in 0..n {
+            for j in 1..(m + 1) {
+                let mut next = i + j;
+                if next > n {
+                    next = 2 * n - (i + j);
+                }
+                dp[kk][next] = (dp[kk][next] + (dp[kk - 1][i] * m_rev) % modulo) % modulo;
             }
-            // let val = mod_pow(m, modulo - 2, modulo);
         }
+        // 以下のやり方だとACできない。(理由は分からない)
+        // for i in 0..(n + 1) {
+        //     // 左から来るケース
+        //     for j in 1..(m + 1) {
+        //         if i >= j {
+        //             dp[kk][i] = (dp[kk][i] + (dp[kk - 1][i - j] * m_rev) % modulo) % modulo;
+        //         }
+        //     }
+        //     // 右から来るケース
+        //     // 次の位置 = N - (今の位置 + 出た目 - N)
+        //     //          = 2N - 今の位置 - 出た目
+        //     // 出た目 = 2N - 今の位置 - 次の位置 で、
+        //     // 1 <= 出た目 <= m の制約がある
+        //     for j in (i + 1)..n {
+        //         if 2 * n <= m + i + j && 1 + i + j <= 2 * n {
+        //             dp[kk][i] = (dp[kk][i] + (dp[kk - 1][j] * m_rev) % modulo) % modulo;
+        //         }
+        //     }
+        // }
+    }
+    let mut ans = 0;
+    for kk in 0..k {
+        ans = (ans + dp[kk][n]) % modulo;
+        // println!("{:?}", dp[kk]);
     }
 
-    println!("{:?}", dp[0]);
-
-
+    println!("{}", ans);
 }
 
 fn mod_pow(mut base: usize, mut exponent: usize, modulo: usize) -> usize {
@@ -70,5 +100,5 @@ fn mod_pow(mut base: usize, mut exponent: usize, modulo: usize) -> usize {
         base = (base * base) % modulo;
         exponent /= 2;
     }
-    return remainder
+    return remainder;
 }

8bca2464a9b3b815d538ada4c1b96e6c341c8bc0
“AkihiroSasabe” <akki.edelweiss@gmail.com>
2022-12-20 21:49:41 +0900

    abc258 d

--- src/abc258/d.rs
+++ src/abc258/d.rs
@@ -9,6 +9,27 @@ use proconio::marker::Chars;
 use std::f64::consts::PI;
 fn main() {
     input! {
-        
+        n: usize,
+        x: usize,
     }
+    let mut a = vec![];
+    let mut b = vec![];
+    for i in 0..n {
+        input! {
+            a_i: usize,
+            b_i: usize
+        }
+        a.push(a_i);
+        b.push(b_i);
+    }
+    let INF = std::usize::MAX;
+    let mut ans = INF;
+    let mut first_term = 0;
+    for i in 0..n {
+        first_term += a[i] + b[i];
+        if x < i + 1 {continue}
+        ans = min(ans, first_term + (x-1-i)* b[i]);
+    }
+    println!("{}", ans);
+
 }

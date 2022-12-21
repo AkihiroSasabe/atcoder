ff2df9ee329c78d4b4cb624dc10dc513be091ecd
“AkihiroSasabe” <akki.edelweiss@gmail.com>
2022-12-21 23:56:41 +0900

    abc 257d continue

--- src/abc257/d.rs
+++ src/abc257/d.rs
@@ -19,17 +19,39 @@ fn main() {
         input! {
             x_i: isize,
             y_i: isize,
-            p_i: usize
+            p_i: isize
         }
         x.push(x_i);
         y.push(y_i);
         p.push(p_i);
     }
 
+    // ダイクストラ法で解く
+    // 最短経路問題
+    // a->b->c->d
+    // ただの幅優先探索で解けそう
+    let mut graph = vec![vec![]; n];
+
+    // let mut s = 0;
     for i in 0..n {
         for j in 0..n {
-            if 
+            if i == j {continue}
+            let dist = ((x[i] - x[j]).abs() + (y[i] - y[j]).abs());
+            println!("i:{} => j: {}, dist:{}, p[i]: {}", i, j, dist, p[i]);
+            let mut kuriage = 0;
+            if dist % p[i] != 0 {
+                kuriage = 1;
+            }
+            let weight = dist / p[i] + kuriage;
+            graph[i].push(vec![j, weight as usize]);
+            // s = max(s, dist / p[i] + kuriage);
         }
     }
+    for i in 0..n {
+        
+        graph
+    }
+    println!("{}", s);
+    
     
 }

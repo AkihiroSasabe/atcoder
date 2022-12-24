b17449ee41f39bbe24a658f74f9a2f284a7ebe15
“AkihiroSasabe” <akki.edelweiss@gmail.com>
2022-12-24 20:42:40 +0900

    abc250 d continue

--- src/abc250/d.rs
+++ src/abc250/d.rs
@@ -4,23 +4,52 @@ fn main() {
         n: usize
     }
 
-    let mut dp = vec![vec![]; n + 1];
+    // p * q^3 <= Nなので、q<N^(1/3)
+    let q_cubic_root = (n as f64).powf(1.0/3.0); 
+    let primes = sieve_oferatosthenes(q_cubic_root as usize);
 
 
-    let mut ans = 0;
-    for i in 2..(n+1) {
-        let primes = prime_factorize(i);
-        if primes.len() == 2 {
-            if primes[0][1] == 1 && primes[1][1] == 3 {
-                ans += 1;
-            }
-        }
-        // println!("i: {}", i);
-    }
+    // let mut dp = vec![vec![]; n + 1];
+
+
+    // let mut ans = 0;
+    // for i in 2..(n+1) {
+    //     let primes = prime_factorize(i);
+    //     if primes.len() == 2 {
+    //         if primes[0][1] == 1 && primes[1][1] == 3 {
+    //             ans += 1;
+    //         }
+    //     }
+    //     // println!("i: {}", i);
+    // }
 
     println!("{}", ans);
 }
 
+// エラトステネスの篩(ふるい)
+// n以下の素数を全て列挙する為のアルゴリズムO(n*log(log(n)))
+fn sieve_oferatosthenes(n: usize) -> Vec<usize> {
+    // let mut root_n = 1;
+    // while root_n * root_n <= n {
+    //     root_n += 1;
+    // }
+    // let mut sosu_list = vec![1; root_n + 1];
+    let mut sosu_list = vec![1; n + 1];
+    sosu_list[0] = 0;
+    sosu_list[1] = 0;
+
+    let mut sosu = 2;
+    
+    for i in 2..(n+1) {
+        if sosu_list[i] == 0 {continue}
+        if n % i == 0 {
+            for j in 2..(n/i) {
+                sosu_list[j * i] = 0;
+            }
+        }
+    }
+    return sosu_list;
+}
 
 fn prime_factorize(n: usize) -> Vec<Vec<usize>> {
     let mut primes = vec![];

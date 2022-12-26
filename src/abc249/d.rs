362e4db99640a75916f8120f42a22cfb1b167f8d
AkihiroSasabe <akki.edelweiss@gmail.com>
2022-12-27 08:17:55 +0900

    Add abc249d continue

--- src/abc249/d.rs
+++ src/abc249/d.rs
@@ -14,24 +14,43 @@ fn main() {
         mut a: [usize; n]
     }
     // a[i] == a[j] * a[k];
-    a.sort();
-    println!("{:?}", a);
-    let mut hash = vec![vec![]; 100_001];
+
+
+    // println!("{:?}", a);
+    let a_max = 100_000;
+    let mut hash = vec![vec![]; a_max+1];
+    // let mut hash = vec![0; a_max+1];
+
     for i in 0..n {
         hash[a[i]].push(i);
+        // hash[a[i]] = 1;
     }
+
     let mut ans = 0;
-    // 2分探索で探す。無理だ。N^2かかる (j,k)のペアを決めるだけで無理
-    // 1がいると話がややこしくなる
-    for i in 0..n {
-        for j in 0..n {
-            if i == j {continue;}
-            let product = a[i]*a[j];
-            if product < 100_001 {
-                ans += hash[product].len();
+    for x in 1..a_max+1 {
+        for y in 1..(a_max/x+1) {
+            let z = x * y;
+            if hash[x].len() > 0 && hash[y].len() > 0 && hash[z].len() > 0 {
+                ans += (hash[x].len() * hash[y].len() * hash[z].len());
             }
         }
     }
+
+
+
+
+    // let mut ans = 0;
+    // // 2分探索で探す。無理だ。N^2かかる (j,k)のペアを決めるだけで無理
+    // // 1がいると話がややこしくなる
+    // for i in 0..n {
+    //     for j in 0..n {
+    //         if i == j {continue;}
+    //         let product = a[i]*a[j];
+    //         if product < 100_001 {
+    //             ans += hash[product].len();
+    //         }
+    //     }
+    // }
     println!("{}", ans);
 
 

2172193006ead160fb498c650667ebad0c0d0c77
“AkihiroSasabe” <akki.edelweiss@gmail.com>
2022-12-26 23:35:36 +0900

    abc249 d continue

--- src/abc249/d.rs
+++ src/abc249/d.rs
@@ -7,39 +7,62 @@
 
 use proconio::input;
 
+// 2022/12/26 22:12~22:17
 fn main() {
     input! {
         n: usize,
-        a: [usize; n]
+        mut a: [usize; n]
     }
-    // println!("a {:?}", a);
-
-    let mut a2 = a.clone();
-
-    // 大きい順にソート
-    a2.sort_by(|x, y| y.cmp(x));
-    // println!("a2 {:?}", a2);
-    let a_max = a2[0];
-
-    // 各要素が何個あるか?
-    let mut counter = vec![0; a_max+1];
-    for i in a.iter() {
-        counter[*i] += 1;
+    // a[i] == a[j] * a[k];
+    a.sort();
+    println!("{:?}", a);
+    let mut hash = vec![vec![]; 100_001];
+    for i in 0..n {
+        hash[a[i]].push(i);
     }
-    // for (i, x) in counter.iter().enumerate() {
-    //     println!("i: {}, x: {}", i, x);
-    // }
-
-    let mut answer = 0;
-    for q in 1..a_max+1 {
-        // println!("q {}", q);
-        for r in 1..(a_max / q + 1) {
-            // println!("r {}", r);
-            let p = q * r;
-            // println!("p, q, r: {} {} {}", p, q, r);
-            // println!("counter[p], counter[q], counter[r]: {} {} {}", counter[p], counter[q], counter[r]);
-            answer += counter[p] * counter[q] * counter[r];
+    let mut ans = 0;
+    // 2分探索で探す。無理だ。N^2かかる (j,k)のペアを決めるだけで無理
+    // 1がいると話がややこしくなる
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {continue;}
+            let product = a[i]*a[j];
+            if product < 100_001 {
+                ans += hash[product].len();
+            }
         }
     }
-    println!("{}", answer);
+    println!("{}", ans);
+
+
+    // // println!("a {:?}", a);
+
+    // let mut a2 = a.clone();
+
+    // // 大きい順にソート
+    // a2.sort_by(|x, y| y.cmp(x));
+    // // println!("a2 {:?}", a2);
+    // let a_max = a2[0];
+
+    // // 各要素が何個あるか?
+    // let mut counter = vec![0; a_max+1];
+    // for i in a.iter() {
+    //     counter[*i] += 1;
+    // }
+    // // for (i, x) in counter.iter().enumerate() {
+    // //     println!("i: {}, x: {}", i, x);
+    // // }
+
+    // let mut answer = 0;
+    // for q in 1..a_max+1 {
+    //     // println!("q {}", q);
+    //     for r in 1..(a_max / q + 1) {
+    //         // println!("r {}", r);
+    //         let p = q * r;
+    //         // println!("p, q, r: {} {} {}", p, q, r);
+    //         // println!("counter[p], counter[q], counter[r]: {} {} {}", counter[p], counter[q], counter[r]);
+    //         answer += counter[p] * counter[q] * counter[r];
+    //     }
+    // }
+    // println!("{}", answer);
 }

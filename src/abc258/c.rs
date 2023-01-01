a40adc80e1feadbc46e84f41aa6986b149b6570a
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-01-01 16:08:33 +0900

    Add abc258 c

--- src/abc258/c.rs
+++ src/abc258/c.rs
@@ -8,7 +8,31 @@ use std::collections::HashMap;
 use proconio::marker::Chars;
 use std::f64::consts::PI;
 fn main() {
+    // 23-01-01 15:47-16:02 => 15min
     input! {
-        
+        n: usize,
+        q: usize,
+        s: Chars,
     }
+    let mut query = vec![];
+    for i in 0..q {
+        input! {
+            t_i: usize,
+            x_i: usize
+        }
+        query.push(vec![t_i, x_i]);
+    }
+    let mut head = 0;
+    for i in 0..q {
+        let t_i = query[i][0];
+        let x_i = query[i][1];
+        if t_i == 1 {
+            head = (n + head - x_i) % n;
+        }
+        else if t_i == 2 {
+            println!("{}", s[(head + x_i - 1) % n]);
+        }
+    }
+
+
 }

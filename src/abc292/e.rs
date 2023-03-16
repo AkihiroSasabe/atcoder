6349f812d5ee631438cc88532d5eb56ab8fab1da
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-03-16 20:51:06 +0900

    abc292 e

--- src/abc292/e.rs
+++ src/abc292/e.rs
@@ -19,7 +19,6 @@ fn main() {
         m: usize,
     }
     let mut graph = vec![vec![]; n];
-    // let mut uft = UnionFindTree::new(n);
     let mut out_edge = vec![0_usize; n];
     let mut in_edge = vec![0_usize; n];
     let mut g_hash = vec![HashMap::new(); n];
@@ -32,29 +31,54 @@ fn main() {
         }
         graph[u_i-1].push(v_i-1);
         g_hash[u_i-1].insert(v_i-1, 0);
-        // uft.unite(u_i-1, v_i-1);
         out_edge[u_i-1] += 1;
         in_edge[v_i-1] += 1;
         uu.push(u_i-1);
         vv.push(v_i-1);
     }
+
     let mut ans = 0;
-
-    let mut ii = 0;
-    while ii < uu.len()  {
-        for j in 0..graph[vv[ii]].len() {
-            let term = graph[vv[ii]][j];
-            if !g_hash[uu[ii]].contains_key(&term) && term != uu[ii] {
-                uu.push(uu[ii]);
-                vv.push(term);
-                ans += 1;
-                g_hash[uu[ii]].insert(term, 0);
-                graph[uu[ii]].push(term);
-                // println!("add: {} -> {}", uu[ii]+1, term+1);
-            }
-        }
-        ii += 1;
+    for v in 0..n {
+        let mut seen = vec![false; n];
+        dfs(&g_hash, v, &mut ans, &mut seen, v);
     }
+    println!("{}", ans);
+
+    let tekito = std::u128::MAX;
+    println!("{}", tekito);
 
 
+    // // 計算量が エッジ数(外側ループwhile) x 頂点数(内側ループ)となる。
+    // // エッジは増えていくので、最大でN*(N-1)/2個になってしまう。これではO(N^3=8*10^9)でTLE
+    // // v0->v1->v2のとき、v0->v2が存在するようにする
+    // // ei := edge の index
+    // let mut ei = 0;
+    // while ei < uu.len()  {
+    //     let v0 = uu[ei];
+    //     let v1 = vv[ei];
+    //     for j in 0..graph[v1].len() {
+    //         let v2 = graph[v1][j];
+    //         if !g_hash[v0].contains_key(&v2) && v2 != v0 {
+    //             uu.push(v0);
+    //             vv.push(v2);
+    //             ans += 1;
+    //             g_hash[v0].insert(v2, 0);
+    //             graph[v0].push(v2);
+    //             // println!("add: {} -> {}", v0+1, v2+1);
+    //         }
+    //     }
+    //     ei += 1;
+    // }
+}
+
+// root_vから到達可能でかつ距離が2以上の頂点の個数を数える。
+fn dfs(graph: &Vec<HashMap<usize, usize>>, v: usize, ans: &mut usize, seen: &mut Vec<bool>, root_v: usize) {
+    seen[v] = true;
+    for (&next_v, _) in &graph[v] {
+        if seen[next_v] {continue}
+        if !graph[root_v].contains_key(&next_v) {
+            *ans += 1;
+        }
+        dfs(graph, next_v, ans, seen, root_v);
+    }
 }

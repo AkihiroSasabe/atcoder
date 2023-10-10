34b4d79fba0f24739f22eded01c3355ae29a7289
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-10-10 19:47:25 +0900

    abc298-e

--- src/abc298/e.rs
+++ src/abc298/e.rs
@@ -14,7 +14,121 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2023-10-10 12:16-12:52 (36min)
+    // 2023-10-10 19:22-19:37 (15min)
+    // total 51min
     input! {
-        
+        n: usize,
+        a: usize,
+        b: usize,
+        p: usize,
+        q: usize,
     }
+    let modulus = 998244353;
+
+    let t_max = 101;
+    // dp[t][x] := 時刻tに、xに居る確率
+    let mut dpa = vec![vec![0; n+1]; t_max+1];
+    let mut dpb = vec![vec![0; n+1]; t_max+1];
+    dpa[0][a] = 1;
+    dpb[0][b] = 1;
+    // println!("dpa[{}] = {:?}", 0, dpa[0]);
+    // println!("dpb[{}] = {:?}", 0, dpb[0]);
+
+    let p_inv = mod_inverse(p, modulus);
+    let q_inv = mod_inverse(q, modulus);
+    // println!("p_inv = {:?}", p_inv);
+    // println!("q_inv = {:?}", q_inv);
+    for t in 0..t_max {
+        for x in 0..n+1 {
+            for ai in 1..p+1 {
+                dpa[t+1][min(x+ai, n)] += dpa[t][x] * p_inv % modulus;
+                dpa[t+1][min(x+ai, n)] %= modulus;
+            }
+            for bi in 1..q+1 {
+                dpb[t+1][min(x+bi, n)] += dpb[t][x] * q_inv % modulus;
+                dpb[t+1][min(x+bi, n)] %= modulus;
+            }
+        }
+        // println!("===================");
+        // println!("dpa[{}] = {:?}", t+1, dpa[t+1]);
+        // println!("dpb[{}] = {:?}", t+1, dpb[t+1]);
+    }
+
+
+    let mut ans = 0;
+    // dpb_under_n[t] := t秒後にB君がNにいない確率
+    let mut dpb_under_n = vec![0; t_max+1];
+    for t in 0..t_max {
+        for x in 0..n {
+            dpb_under_n[t] += dpb[t][x];
+            dpb_under_n[t] %= modulus;
+        }
+        if t > 0 {
+            // ゲームが続く確率をかけないといけないな。
+            // t秒後に初めてA君(高橋君)がNにいて、t-1秒後にB君(アオキ君)がNにいない確率
+            let diff = (modulus + dpa[t][n] - dpa[t-1][n]) * dpb_under_n[t-1] % modulus;
+            ans += diff;
+            // println!("t = {t}, diff = {diff}, ans = {ans}");
+        }
+        ans %= modulus;
+    }
+    println!("{}", ans);
+
+
 }
+
+
+// mod p を法とした時の割り算 a / b の値
+fn mod_dev(a: usize, b: usize, modulo: usize) -> usize {
+    return a * mod_inverse(b, modulo) % modulo
+}
+
+// mod p を法とした時の逆数(逆元という) 1 / b の値
+fn mod_inverse(a: usize, modulo: usize) -> usize {
+    // フェルマーの小定理
+    //     a^(p-1) = 1     (mod p)
+    // <=> a * a^(p-2) = 1 (mod p)
+    // <=> 1 / a = a^(p-2) (mod p)
+    // (ただし、法pは素数)
+
+    return mod_pow(a, modulo - 2, modulo)
+}
+
+// mod p を法とした時の累乗
+// base^(x) % mod を繰り返し二乗法により、O(log2(x))の計算量で求める　(O(x)だとTLE)
+// No.69参照
+fn mod_pow(mut base: usize, mut exponent: usize, modulo: usize) -> usize {
+    // 例: 3^4= (3^2)^2 = 9^2 = 81^1
+    // 初期
+    // 3^4
+    // remainder=1
+    // base=3
+    // exp=4
+
+    // i=0:
+    // remainder = 1
+    // base = 3 * 3 = 9
+    // exp = 4 / 2 = 2
+
+    // i=1:
+    // remainder = 1
+    // base = 9 * 9 = 81
+    // exp = 2 / 2 = 1
+
+    // i=2:
+    // remainder = remainder * base = 81
+    // base = 81 * 81
+    // exp = 1 / 2 = 0
+
+    let mut remainder = 1;
+    while exponent != 0 {
+        if exponent % 2 == 1 {
+            remainder = (remainder * base) % modulo;
+        }
+        base = (base * base) % modulo;
+        exponent /= 2;
+    }
+    return remainder
+}
+

9c06e5346b6c91cad3cc78a338a73b015dc44037
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-10-15 18:36:11 +0900

    abc298-e

--- src/abc289/e.rs
+++ src/abc289/e.rs
@@ -14,10 +14,7 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
-    // let mut hash = HashMap::new();
-    // let s = String::from("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-    // hash.insert(s, 1);
-    // println!("{:?}", hash);
+    // 2023-10-14 11:21-13:02 (1h41m = 101m)
     input! {
         t: usize
     }
@@ -28,7 +25,7 @@ fn main() {
             c: [usize; n],
         }
         let mut graph = vec![vec![]; n];
-        for j in 0..n {
+        for j in 0..m {
             input! {
                 u_j: usize,
                 v_j: usize,
@@ -36,33 +33,48 @@ fn main() {
             graph[u_j-1].push(v_j-1);
             graph[v_j-1].push(u_j-1);
         }
+
+        solve(n, &c, &graph);
     }
 }
 
-// fn dfs(graph: &mut Vec<Vec<usize>>, v: usize, seen: &mut Vec<bool>, c: &Vec<usize>) {
-//     seen[v] = true;
-//     for i in 0..graph[v].len() {
-//         let next_v = graph[v][i];
-//         if seen[next_v] {continue}
-//         dfs(graph, next_v, seen, c);
-//     }
-//     seen[v] = false;
-// }
+fn solve(n: usize, c: &Vec<usize>, graph: &Vec<Vec<usize>>) {
+    if c[0] == c[n-1] {
+        println!("-1");
+        return
+    }
 
-fn dfs(graph: &mut Vec<Vec<usize>>, vt: usize, va: usize, seen_t: &mut Vec<bool>, seen_a: &mut Vec<bool>, c: &Vec<usize>) {
-    seen_t[vt] = true;
-    seen_a[va] = true;
-    
-    for i in 0..graph[vt].len() {
-        for j in 0..graph[va].len() {
-            let vnt = graph[vt][i];
-            let vna = graph[va][j];
-            if seen_t[vnt] {continue}
-            if seen_a[vna] {continue}
-            if c[vnt] == c[vnc] {continue}
-            dfs(graph, vnt, vna, seen_t, seen_a, c);
-            
+    // BFSで(高橋, 青木) が (0, n-1) -> (n-1, 0) となる最短距離を求める
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+    let mut distance: HashMap<(usize, usize), u32> = HashMap::new();
+    let init_state = (0_usize, n-1);
+    distance.insert(init_state.clone(), 0);
+
+    queue.push_back(init_state);
+    while queue.len() > 0 {
+        let state = queue.pop_front().unwrap();
+        let v0 = state.0;
+        let v1 = state.1;
+
+        let next_distance = *distance.get(&state).unwrap() + 1;
+        
+        for &next_v0 in graph[v0].iter() {
+            for &next_v1 in graph[v1].iter() {
+                if c[next_v0] == c[next_v1] {continue}
+                let next_state = (next_v0, next_v1);
+                if distance.contains_key(&next_state) {continue}
+                distance.insert(next_state.clone(), next_distance);
+                queue.push_back(next_state);
+            }
         }
+    }
+    let final_state = (n-1, 0);
+    if !distance.contains_key(&final_state) {
+        println!("-1");
+    }
+    else {
+        let ans = *(distance.get(&final_state).unwrap());
+        println!("{}", ans);
     }
 
 }

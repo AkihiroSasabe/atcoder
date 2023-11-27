4a843d5495cc9334c80d6259ba573a1796f6a08f
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-11-27 12:33:05 +0900

    abc328 e and f

--- src/abc328/e.rs
+++ src/abc328/e.rs
@@ -14,10 +14,117 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2023-11-23 12:35-13:14 (39min)
     input! {
         n: usize,
         m: usize,
         k: usize,
     }
-    
+    let mut edges = vec![];
+    for i in 0..m {
+        input! {
+            u_i: usize,
+            v_i: usize,
+            w_i: usize,
+        }
+        edges.push((u_i-1, v_i-1, w_i));
+    }
+    // 全域木T
+    // Tのコスト := Tの重み総和 % K
+    // コストの最小値を答えよ
+    // M <= N(N-1)/2 <= 8*7/2 = 28
+    // 2^M = 2^28 = 268_435_456 (重み総和のパターン数)
+
+    // let mut aaa = vec![];
+
+    // 28C7 = 1_184_040
+    let mut ans = k - 1;
+    for comb in (0..m).combinations(n-1) {
+        // println!("{:?}", comb);
+        let mut uft = UnionFindTree::new(n);
+        let mut hash = HashSet::new();
+        let mut is_spanning = true;
+        let mut sum = 0;
+        for i in comb {
+            let (u, v, w) = edges[i];
+            if uft.issame(u, v) {
+                is_spanning = false;
+                break
+            }
+            uft.unite(u, v);
+            hash.insert(u);
+            hash.insert(v);
+            sum += w;
+        }
+        if is_spanning && hash.len() == n {
+            ans = min(ans, sum % k);
+        }
+    }
+    println!("{}", ans);
+    // 11360716373
+    // 8648083103
+}
+
+
+// Union-Find
+// グループの管理（グループ同士の結合や、要素同士の所属か同じか判定）するのに便利なデータ構造
+struct UnionFindTree {
+    parents: Vec<usize>,    // 各頂点の属するグループ(根付き木)の親頂点の番号
+    sizes: Vec<usize>       // 各頂点の属するグループ(根付き木)のサイズ(頂点数)
+}
+
+impl UnionFindTree {
+    // 初期化
+    fn new(n: usize) -> Self {
+        // 各頂点が属するグループの根を格納
+        let parents = (0..n).collect();
+        // 各頂点が属するグループのサイズ(頂点数)を格納。※ただし、更新するのは根のインデックスだけで良い
+        let sizes = vec![1; n];
+        return UnionFindTree {parents, sizes}
+    }
+
+    // 根を求める。経路圧縮により計算量を削減
+    fn root(&mut self, v: usize) -> usize {
+        if self.parents[v] == v {return v}
+        else {
+            // 経路圧縮 (親を根に張り替える。)
+            self.parents[v] = self.root(self.parents[v]);
+            return self.parents[v] as usize
+        }
+    }
+
+    // 同じグループに属するか
+    fn issame(&mut self, v0: usize, v1: usize) -> bool {
+        return self.root(v0) == self.root(v1)
+    }
+
+    // 頂点vが属する根付き木のサイズを取得
+    fn size(&mut self, v: usize) -> usize {
+        let root = self.root(v);
+        return self.sizes[root]
+    }
+
+    // v0を含むグループと、v1を含むグループとを併合する。Union by sizeで計算量削減。
+    fn unite(&mut self, mut v0: usize, mut v1: usize) -> bool {
+        // 既に同じグループであれば何もしない
+        v0 = self.root(v0);
+        v1 = self.root(v1);
+        if v0 == v1 {
+            return false
+        } 
+        let child: usize;
+        let parent: usize;
+        // Union by sizeにより、サイズが小さいグループを、サイズが大きいグループに併合する
+        if self.size(v0) <= self.size(v1) {
+            child = v0;
+            parent = v1;
+        }
+        else {
+            child = v1;
+            parent = v0;
+        }
+        self.sizes[parent] += self.size(child);
+        self.parents[child] = parent;
+        return true
+    }
 }
--- src/abc328/f.rs
+++ src/abc328/f.rs
@@ -14,7 +14,162 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2023-11-23 13:24-
     input! {
+        n: usize,
+        q: usize
+    }
+    let mut a = vec![];
+    let mut b = vec![];
+    let mut d = vec![];
+    for i in 0..q {
+        input! {
+            a_i: usize,
+            b_i: usize,
+            d_i: isize,
+        }
+        a.push(a_i-1);
+        b.push(b_i-1);
+        d.push(d_i);
+    }
+
+    let mut p_uft = PotentialUnionFindTree::new(n);
+    
+    let mut ans = vec![];
+    for i in 0..q {
+        // 以前のマージ結果と整合性があるか?
+        let ok_flag = p_uft.unite(a[i], b[i], d[i]);
+        if ok_flag {
+            ans.push(i + 1);
+        }
+    }
+    for i in 0..ans.len() {
+        print!("{} ", ans[i]);
+    }
+}
+
+
+
+
+// Potential Union-Find-Tree (重み付きUFT): 
+// 各ノードに根からのポテンシャルを定義し、ノード間の距離を管理できるように拡張したUFT
+// ■普通のUFT
+// unite(v0,v1)     頂点v0 を含むグループと 頂点v1 を含むグループをマージする
+// issame(v0,v1)    頂点v0 と 頂点v1 が同じグループにいるかどうかを判定する
+//
+// ■重み付きUFT:
+// unite(v0,v1,w)   potential(v0) - potential(v1) == w となるように結合する。以前の結合結果と整合性があるチェックし、矛盾がない場合だけ結合する。
+// issame(v0,v1)    頂点v0 と 頂点v1 が同じグループにいるかどうかを判定する
+// diff(v0,v1)      v0 と v1 とが同じグループにいるとき、ポテンシャルの差分 potential(v0) - potential(v1) を取得
+// 
+// 裁量
+// ・xとyが既に同じグループにいるとき、unite(x,y,w)を呼んじゃうか? -> 自分のは呼ぶ。ポテンシャルに矛盾があればfalseを返す
+// ・xとyが同じグループにいないときにdiff(x,y)を呼んじゃうか? -> 自分のはNoneを返す
+// 実装方法と内部の処理の理解はけんちょん氏の記事が参考になる　https://qiita.com/drken/items/cce6fc5c579051e64fab
+struct PotentialUnionFindTree {
+    parents: Vec<usize>,    // 各頂点の属するグループ(根付き木)の親頂点の番号
+    sizes: Vec<usize>,      // 各頂点の属するグループ(根付き木)のサイズ(頂点数)
+    potentials: Vec<isize>   // 親頂点から見た子頂点のポテンシャル
+}
+
+impl PotentialUnionFindTree {
+    fn new(n: usize) -> Self {
+        // 各頂点が属するグループの根を格納
+        let parents: Vec<usize> = (0..n).collect();
+
+        // 各頂点が属するグループのサイズ(頂点数)を格納。※ただし、更新するのは根のインデックスだけで良い
+        let sizes: Vec<usize> = vec![1; n];
         
+        // 親頂点から見た子頂点のポテンシャル。
+        // 初期状態では、各頂点は独立しているので、自分自身が親頂点であり、ポテンシャルは0
+        let potentials: Vec<isize> = vec![0; n];
+
+        return PotentialUnionFindTree {parents, sizes, potentials}
+    }
+
+    // 根を求める。経路圧縮により計算量を削減
+    fn root(&mut self, v: usize) -> usize {
+        if self.parents[v] == v {
+            // 頂点vが頂点vの親pと一致したとき、vがそのグループの根である。
+            return v
+        }
+        else {
+            // 頂点vの根 = 親pの根 なので、再帰的に根を求められる
+            let r = self.root(self.parents[v]);
+
+            // vの親を根に張り替える場合、ポテンシャルの原点も、古い親から新しい親に合わせる必要がある。
+            // 古い親自身のポテンシャルを、オフセットすればいい (根のポテンシャルは0なことに注意)
+            self.potentials[v] += self.potentials[self.parents[v]];
+
+            // 経路圧縮 (親を根に張り替える。)
+            self.parents[v] = r;
+            return self.parents[v]
+        }
+    }
+
+    // 頂点vのポテンシャルを取得
+    fn potential(&mut self, v: usize) -> isize {
+        // 経路圧縮 (vの親を、確実に最新の根に張替える。)
+        self.root(v);
+        return self.potentials[v]
+    }
+    // 頂点v0とv1のポテンシャル差: P(v0) - P(v1) を取得
+    fn diff(&mut self, v0: usize, v1: usize) -> Option<isize> {
+        if self.issame(v0, v1) {
+            return Some(self.potential(v0) - self.potential(v1))
+        }
+        else {
+            // v0とv1が違うグループにいるときは、ポテンシャル差が分からないのでNoneで返す
+            return None
+        }
+    }
+
+    // 同じグループに属するか
+    fn issame(&mut self, v0: usize, v1: usize) -> bool {
+        return self.root(v0) == self.root(v1)
+    }
+
+    // 頂点vが属する根付き木のサイズを取得
+    fn size(&mut self, v: usize) -> usize {
+        let root = self.root(v);
+        return self.sizes[root]
+    }
+
+    // potential(v0) - potential(v1) = w となるように 併合 する。(以前の併合結果との整合性をチェックし、矛盾があったら何もしない)
+    // v0を含むグループと、v1を含むグループとを併合する。Union by sizeで計算量削減。
+    fn unite(&mut self, v0: usize, v1: usize, w: isize) -> bool {
+        // 既に同じグループであれば何もしない
+        let r0 = self.root(v0);
+        let r1 = self.root(v1);
+        
+        // r1 からみた、 r0 のポテンシャルの高さ
+        let potential_r0_from_r1 = self.potential(v1) + w - self.potential(v0); // 絵を書くとこの式の意味がよく分かる
+        if r0 == r1 {
+            // v0 と v1 が既に繋がっている場合は何もしない
+            // 以前の併合結果と矛盾する場合は、整合性を false で返す
+            return potential_r0_from_r1 == 0
+        }
+
+        // 統合後の状態
+        let child: usize;
+        let parent: usize;
+        let child_potential: isize; // 結合後の新しい根から見た、取り込まれたグループの旧根のポテンシャル
+        // Union by sizeにより、サイズが小さいグループを、サイズが大きいグループに併合する
+        if self.size(r0) <= self.size(r1) {
+            child = r0;
+            parent = r1;
+            child_potential = potential_r0_from_r1;
+        }
+        else {
+            child = r1;
+            parent = r0;
+            child_potential = - potential_r0_from_r1;
+        }
+        self.sizes[parent] += self.size(child);
+        self.parents[child] = parent;
+        self.potentials[child] = child_potential;
+
+        // 整合性のある併合ができた場合は、trueで返す
+        return true
     }
 }

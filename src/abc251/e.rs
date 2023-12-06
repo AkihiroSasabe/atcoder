634f2c7ba2d5e22b56c8fba4b061fbe11cb7afdd
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-12-06 17:35:35 +0900

    abc251 e

--- src/abc251/e.rs
+++ src/abc251/e.rs
@@ -14,19 +14,43 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2023-12-06 16:49-17:21 (32min)
     input! {
         n: usize,
         a: [usize; n]
     }
-    
-    a1  2   1,2
-    a2  5   2,3
-    a3  3   3,4
-    a4  2   4,5
-    a5  5   5,6
-    a6  1   6,1
-    for i in 0..a.len() {
 
+    const INF: usize = 1 << 60;
+
+    // 1本目を選ばない
+    let mut dp0 = vec![vec![INF; 2]; n];
+    dp0[0][0] = 0;
+
+    // 1本目を選んでいる
+    let mut dp1 = vec![vec![INF; 2]; n];
+    dp1[0][1] = a[0];
+
+    for i in 1..n-1 {
+        // i本目の辺を選ばない
+        dp0[i][0] = dp0[i-1][1];
+        dp1[i][0] = dp1[i-1][1];
+
+        // i本目の辺を選ぶ
+        dp0[i][1] = min(dp0[i-1][0] + a[i], dp0[i-1][1] + a[i]);
+        dp1[i][1] = min(dp1[i-1][0] + a[i], dp1[i-1][1] + a[i]);
     }
+    // println!("dp0 = {:?}", dp0);
+    // println!("dp1 = {:?}", dp1);
 
+    // 最後の辺を選ばない
+    let mut ans = dp1[n-2][1];
+
+    // 最後の辺を選ぶ
+    ans = min(ans, dp0[n-2][0] + a[n-1]);
+    ans = min(ans, dp0[n-2][1] + a[n-1]);
+    ans = min(ans, dp1[n-2][0] + a[n-1]);
+    ans = min(ans, dp1[n-2][1] + a[n-1]);
+
+    println!("{}", ans);
+    
 }

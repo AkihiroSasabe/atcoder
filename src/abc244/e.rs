1ddfc20cebdacbd2897627fec35678fdd997c0e2
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-12-08 17:03:56 +0900

    abc244 e

--- src/abc244/e.rs
+++ src/abc244/e.rs
@@ -6,7 +6,130 @@ use std::collections::VecDeque;
 use std::collections::BinaryHeap;
 use proconio::marker::Chars;
 fn main() {
+    // 2023-12-08 16:06-16:51 (45min)
     input! {
-        
+        n: usize,
+        m: usize,
+        mut k: usize,
+        mut s: usize,
+        mut t: usize,
+        mut x: usize,
+    }
+    s -= 1; // 始点
+    t -= 1; // 終点
+    x -= 1; // 偶数回登場しないといけない、頂点
+    let mut uft = UnionFindTree::new(n);
+
+    const MODULUS: usize = 998_244_353;
+    let mut graph = vec![vec![]; n];
+    for i in 0..m {
+        input! {
+            u_i: usize,
+            v_i: usize,
+        }
+        graph[u_i - 1].push(v_i - 1);
+        graph[v_i - 1].push(u_i - 1);
+        uft.unite(u_i - 1, v_i - 1);
+    }
+
+    if !uft.issame(s, t) {
+        println!("0");
+        return;
+    }
+
+    // dp[i][v][偶奇] := a[i] = v で x が 偶数回 or 奇数回　登場している場合の数の総数
+    let mut dp = vec![vec![vec![0; 2]; n]; k+1];
+    dp[0][s][0] = 1;
+
+
+    for i in 0..k {
+        for v in 0..n {
+            for &nv in graph[v].iter() {
+                if nv == x {
+                    dp[i+1][nv][0] += dp[i][v][1];
+                    dp[i+1][nv][1] += dp[i][v][0];
+                }
+                else {
+                    dp[i+1][nv][0] += dp[i][v][0];
+                    dp[i+1][nv][1] += dp[i][v][1];
+                }
+                dp[i+1][nv][0] %= MODULUS;
+                dp[i+1][nv][1] %= MODULUS;
+            }
+        }
+    }
+    for i in 0..k+1 {
+        // println!("i = {i}");
+        for v in 0..n {
+            // print!("{:?}", dp[i][v]);
+        }
+        // println!("");
+    }
+    // println!("dp = {:?}", dp);
+    println!("{}", dp[k][t][0]);
+
+}
+
+
+// Union-Find
+// グループの管理（グループ同士の結合や、要素同士の所属か同じか判定）するのに便利なデータ構造
+struct UnionFindTree {
+    parents: Vec<usize>,    // 各頂点の属するグループ(根付き木)の親頂点の番号
+    sizes: Vec<usize>       // 各頂点の属するグループ(根付き木)のサイズ(頂点数)
+}
+
+impl UnionFindTree {
+    // 初期化
+    fn new(n: usize) -> Self {
+        // 各頂点が属するグループの根を格納
+        let parents = (0..n).collect();
+        // 各頂点が属するグループのサイズ(頂点数)を格納。※ただし、更新するのは根のインデックスだけで良い
+        let sizes = vec![1; n];
+        return UnionFindTree {parents, sizes}
+    }
+
+    // 根を求める。経路圧縮により計算量を削減
+    fn root(&mut self, v: usize) -> usize {
+        if self.parents[v] == v {return v}
+        else {
+            // 経路圧縮 (親を根に張り替える。)
+            self.parents[v] = self.root(self.parents[v]);
+            return self.parents[v] as usize
+        }
+    }
+
+    // 同じグループに属するか
+    fn issame(&mut self, v0: usize, v1: usize) -> bool {
+        return self.root(v0) == self.root(v1)
+    }
+
+    // 頂点vが属する根付き木のサイズを取得
+    fn size(&mut self, v: usize) -> usize {
+        let root = self.root(v);
+        return self.sizes[root]
+    }
+
+    // v0を含むグループと、v1を含むグループとを併合する。Union by sizeで計算量削減。
+    fn unite(&mut self, mut v0: usize, mut v1: usize) -> bool {
+        // 既に同じグループであれば何もしない
+        v0 = self.root(v0);
+        v1 = self.root(v1);
+        if v0 == v1 {
+            return false
+        } 
+        let child: usize;
+        let parent: usize;
+        // Union by sizeにより、サイズが小さいグループを、サイズが大きいグループに併合する
+        if self.size(v0) <= self.size(v1) {
+            child = v0;
+            parent = v1;
+        }
+        else {
+            child = v1;
+            parent = v0;
+        }
+        self.sizes[parent] += self.size(child);
+        self.parents[child] = parent;
+        return true
     }
 }

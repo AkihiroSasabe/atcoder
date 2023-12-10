185fe927f748c5edbfdd9ff91fcb8d55f43061ae
AkihiroSasabe <akki.edelweiss@gmail.com>
2023-12-10 20:50:31 +0900

    abc216 c

--- src/abc216/c.rs
+++ src/abc216/c.rs
@@ -14,7 +14,52 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2023-12-09 23:46-
     input! {
-        
+        n: usize,
     }
+
+    // 
+    // 5
+    // 101 <- +1, *2, *2, +1
+    // 13
+    // 1101 <- +1, *2, +1, *2, +1
+
+    // 1    +1
+    // 10   *2
+    // 11   +1
+    // 110  *2
+    // 1100 *2
+    // 1101 +1
+
+    // 101
+    // 1 +1
+    // 10 *2
+    // 100 *2
+    // 101 +1
+
+    // 1152921504606846976
+    // 1000000000000000000
+
+    let mut ans = vec![];
+    let mut flag = false;
+    for i in (0..61).rev() {
+        if 1 << i & n != 0 {
+            ans.push('A');
+            flag = true;
+        }
+        if flag {
+            if i != 0 {
+                ans.push('B');
+            }
+        }
+    }
+
+    for i in 0..ans.len() {
+        print!("{}", ans[i]);
+    }
+
+
+
+
 }

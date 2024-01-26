a7de160a72a5d6bc176843c0a771abc804261812
AkihiroSasabe <akki.edelweiss@gmail.com>
2024-01-26 21:05:26 +0900

    abc221 e

--- src/abc221/e.rs
+++ src/abc221/e.rs
@@ -14,7 +14,314 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2024-01-25 19:56-20:34 (38min, 悩む)
+    // 2024-01-25 20:34-21:07 (33min, 解説ac)
+    // total: 71min
     input! {
-        
+        n: usize,
+        a: [usize; n]
     }
+    let modulus: usize = 998_244_353;
+
+    // 2^x, 2^(-x) を事前に求める。(x=0,1,2,..n+1)
+    let mut pow_2 = vec![0; n+2];
+    let mut pow_i2 = vec![0; n+2];
+    let mut p2 = 1;
+    let mut pi2 = 1;
+    let inv2 = mod_inverse(2, modulus);
+    for i in 0..n+2 {
+        pow_2[i] = p2;
+        pow_i2[i] = pi2;
+
+        p2 *= 2;
+        p2 %= modulus;
+
+        pi2 *= inv2;
+        pi2 %= modulus;
+    }
+
+    // aを順位変換
+    let (ranked_array, _, _) = rank_array(&a);
+
+    // 例: n=7 で、
+    // ind:0, 1, 2, 3, 4, 5, 6
+    // A = 2, 1, 2, 1, 3, 4, 3
+    // のとき、
+    // A[6]=3 に着目すると、i=0,1,2,...,5 の中から,答えに寄与するのは、
+    // 2^5 + 2^4 + 2^3 + 2^2 + 2^1 + 2^0 のうち、
+    // 2^5 + 2^4 + 2^3 + 2^2 + 2^1 である。
+    // = 2^5*(2^0 + 2^-1 + 2^-2 + 2^-3 + 2^-4)
+    
+    // A[5]=4 に着目すると、i=0,1,2,...,4 の中から,答えに寄与するのは、
+    // 2^4 + 2^3 + 2^2 + 2^1 + 2^0 
+    // = 2^4*(2^0 + 2^-1 + 2^-2 + 2^-3 + 2^-4)
+
+    // A[4]=3 に着目すると、i=0,1,2,...,3 の中から,答えに寄与するのは、
+    // 2^3 + 2^2 + 2^1 + 2^0 
+    // = 2^3*(2^0 + 2^-1 + 2^-2 + 2^-3)
+    
+    // よって、Aのうち、小さい数のものから、足していけば良い。(値が同じ場合は、左から考える)
+    // i == i のときの、
+    // BIT := A[i]より小さい項A[j]であるとき、T[i][j] == 1 (そうでなければ0)とすると、BITのi番目の項目には、
+    // (T[i][0]*2^0 + T[i][1]*2^-1 + T[i][2]*2^-2 + T[i][3]*2^-3 + T[i][4]*2^-4)
+    // が格納される。
+    
+
+    let mut bit = BinaryIndexedTree::new(n);
+
+    // spe: [順位, index]を、昇順に格納した配列
+    let mut spe = vec![];
+    for i in 0..n {
+        spe.push([ranked_array[i], i]);
+    }
+    spe.sort();
+    let mut ans = 0;
+    for i in 0..n {
+        let rank = spe[i][0];
+        let ind = spe[i][1];
+        if ind != 0 {
+            let diff = (bit.sum(ind-1) % modulus) * pow_2[ind - 1] % modulus;
+            ans = ans + diff;
+            ans %= modulus; 
+        }
+        bit.add(ind, pow_i2[ind]);
+    }
+    println!("{}", ans);
+
+
+
+
+}
+
+
+
+// mod p を法とした時の割り算 a / b の値
+fn mod_dev(a: usize, b: usize, modulo: usize) -> usize {
+    return a % modulo * mod_inverse(b, modulo) % modulo
+}
+
+// mod p を法とした時の逆数(逆元という) 1 / b の値
+fn mod_inverse(a: usize, modulo: usize) -> usize {
+    // フェルマーの小定理
+    //     a^(p-1) = 1     (mod p)
+    // <=> a * a^(p-2) = 1 (mod p)
+    // <=> 1 / a = a^(p-2) (mod p)
+    // ただし、法pは素数で、aはpの倍数ではない整数。
+    // aがpの倍数だと、a^(p-1)=0 (mod p)となる。
+
+    return mod_pow(a % modulo, modulo - 2, modulo)
+}
+
+// mod p を法とした時の累乗
+// base^(x) % mod を繰り返し二乗法により、O(log2(x))の計算量で求める　(O(x)だとTLE)
+// No.69参照
+fn mod_pow(mut base: usize, mut exponent: usize, modulo: usize) -> usize {
+    // 例: 3^4= (3^2)^2 = 9^2 = 81^1
+    // 初期
+    // 3^4
+    // remainder=1
+    // base=3
+    // exp=4
+
+    // i=0:
+    // remainder = 1
+    // base = 3 * 3 = 9
+    // exp = 4 / 2 = 2
+
+    // i=1:
+    // remainder = 1
+    // base = 9 * 9 = 81
+    // exp = 2 / 2 = 1
+
+    // i=2:
+    // remainder = remainder * base = 81
+    // base = 81 * 81
+    // exp = 1 / 2 = 0
+
+    base %= modulo;
+    let mut remainder = 1;
+    while exponent != 0 {
+        if exponent % 2 == 1 {
+            remainder = (remainder * base) % modulo;
+        }
+        base = (base * base) % modulo;
+        exponent /= 2;
+    }
+    return remainder
+}
+
+
+
+/// Binary Indexed Tree (BIT)
+/// 参考: https://algo-logic.info/binary-indexed-tree/
+/// (1)構築: O(N)
+/// (2)加算: O(logN): 数列Anのi番目の項にxを足す (区間加算じゃないので注意)
+/// (3)区間和: O(logN): 数列Anの先頭からi番目までの項の和を求める (閉区間だからiも含めるので注意)
+/// セグメント木より機能が限定的だが、実装が簡単 & 定数倍で高速 & 省メモリ
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct BinaryIndexedTree<T> {
+    n: isize,       // 配列の要素数(数列の要素数+1)
+    bit: Vec<T>    // データの格納先(1-indexed)。初期値は0
+    // 0 始まり(0-indexed) ではなく 1 から番号を始めている(1-indexed)
+    // また半開区間ではなく閉区間で考える。
+    // これは後で計算をする際に楽になるため。
+}
+
+impl<T: Default + Copy + std::ops::AddAssign + std::ops::SubAssign + std::fmt::Debug + std::ops::Sub<Output = T>> BinaryIndexedTree<T> {
+    fn new(n: usize) -> Self {
+        BinaryIndexedTree {
+            n: (n + 1) as isize,
+            bit: vec![T::default(); n + 1] // 例えばTがusizeならdefault()は0を返す
+        }
+    }
+
+    // add のインターフェースとしてindexは元の数列のindexを採用している(内部で+1している)
+    fn add(&mut self, index: usize, x: T) {
+        let mut i = (index + 1) as isize;
+        // let mut i = index as isize; // こっちを採用すると、インターフェースも半開区間にできる
+        while i < self.n {
+            self.bit[i as usize] += x;
+            // println!("i={}, i={:05b} -i={:05b}", i, i, -i);
+
+            // i の最後の1のビット = i & -i (∵負の数は「ビット反転+1」で表現される)
+            // 例: 6 & -6 = (00…0110)_2 & (11…1010)_2 = (00…0010)_2
+            i += (i & - i); // iにi の最後の1のビットを足すと、親のインデックスに移れる
+
+            // Rustでは、負の数は2の補数表現で保持される。
+            // 補数の定義: N進法において自然数xを表現するのに必要な最小の桁数をnとしたとき
+            // xのNの補数はN^n - x となる
+            // 例： 5(10進数)=101(2進数)の2の補数は、2^3-5(10進法) = 1000 - 101 (2進法) = 011(2進法)となる
+            // 参考1: http://www.cc.kyoto-su.ac.jp/~kbys/kiso/number/negative.html
+            // 参考2: http://www.f.waseda.jp/takezawa/math/joho/hosuu.pdf
+            // もっと端的に言うと、
+            // 0の定義を、そのデータ型のビット数の限界に1桁左から1を追加したものとする
+            // 例: 3bitだけ使える場合、下記のように考える
+            // -3: 0101
+            // -2: 0110
+            // -1: 0111
+            //  0: 1000 <- 3bitの0の定義
+            //  1: 0001
+            //  2: 0010
+            //  3: 0011
+            // また、isizeの場合、-1は
+            // 1111111111
+            // 1111111111
+            // 1111111111
+            // 1111111111
+            // 1111111111
+            // 1111111111
+            // 1111 となる (1が64個)。
+        }
+    }
+
+    // Tが非負整数型(usizeなど)のときに、除算を行う
+    fn subtract(&mut self, index: usize, x: T) {
+        let mut i = (index + 1) as isize;
+        // let mut i = index as isize; // こっちを採用すると、インターフェースも半開区間にできる
+        while i < self.n {
+            self.bit[i as usize]  -= x;
+            i += (i & - i); // iにi の最後の1のビットを足すと、親のインデックスに移れる
+        }
+    }
+
+    // a_1 + a_2 + ... + a_i を計算する (sumのインターフェースは半開区間ではなく閉区間を採用。a[index]は足される)
+    fn sum(&self, index: usize) -> T {
+        let mut i = (index + 1) as isize;
+        // let mut i = index as isize; // こっちを採用すると、インターフェースも開区間にできる
+        let mut sum = T::default(); // 例えばTがusizeならdefault()は0を返す
+        while i > 0 {
+            // println!("i={}, sum={:?}", i, sum);
+            sum += self.bit[i as usize];
+            // i の最後の1のビット = i & -i (∵負の数は「ビット反転+1」で表現される)
+            // 例: 6 & -6 = (00…0110)_2 & (11…1010)_2 = (00…0010)_2
+            i -= (i & - i); // iにi の最後の1のビットを引くと、1個左上のノードのインデックスに移れる
+            // println!("i={}, sum={:?}", i, sum);
+            // println!("==== ==== ==== ====");
+        }
+        return sum
+    }
+    // 閉区間[left, right]を取得する
+    fn sum_range(&self, left: usize, right: usize) -> T {
+        let right_sum: T = self.sum(right);
+        let left_sum: T = match left == 0 {
+            true => Default::default(), // 0のこと
+            false => self.sum(left-1)
+        };
+        let range_sum: T = right_sum - left_sum;
+        return range_sum
+    }
+    // index番目の値を取得する (sum()は累積和を取得するメソッド)
+    fn get_element(&self, index: usize) -> T {
+        let element = match index == 0 {
+            true => self.sum(index),
+            false => self.sum(index) - self.sum(index - 1)
+        };
+        return element
+    }
+    fn print_all_cum(&self) {
+        // デバッグ用に、各インデックスにおける、累積和を標準出力に print
+        print!("bit = ");
+        for i in 0..self.n-1 {
+            let sum_i = self.sum(i as usize);
+            print!("{:?} ", sum_i);
+        }
+        println!("");
+    }
+}
+
+
+
+fn rank_array<T: Ord + std::hash::Hash + Clone + Copy>(array: &Vec<T>) -> (Vec<usize>, Vec<T>, HashMap<T, usize>) {
+    // 配列を順位変換する関数 O(NlogN)
+    // 要素の値を圧縮することを、目的として使うことを想定している。
+    // Input: 
+    //     array: 配列
+    // Output: 
+    //    ranked_array:     順位変換済み配列
+    //    sorted_array:     ソート済みの配列(順位から元の値をマップさせる)
+    //    original_to_rank: 元の値から順位を対応させるマップ
+    // Example:
+    // let array = vec![333, 111, 444, 111, 555, 999];
+    // let (ranked_array, _, _) = rank_array(&array);
+    // assert_eq!(ranked_array, vec![2, 0, 3, 0, 4, 5]);
+
+    // 配列のサイズ
+    let n = array.len();
+
+    // B木<数列中に登場する値, 頻度>
+    let mut btree: BTreeMap<T, usize> = BTreeMap::new();
+    for i in 0..n {
+        *(btree.entry(array[i]).or_insert(0)) += 1;
+    }
+
+    // 昇順ソート済みの、順位変換済み配列
+    let mut sorted_rank_array = vec![];
+    let mut rank = 0;
+    for (k, frequency) in btree {
+        for j in 0..frequency {
+            sorted_rank_array.push(rank);
+        }
+        rank += frequency; // sorted_rank_array = [0, 0, 2, 3, 4, 5], 
+        // ここを1にすると、隙間なくなる。
+        // rank += 1; //sorted_rank_array = [0, 0, 1, 2, 3, 4], 
+    }
+    // println!("sorted_rank_array = {:?}, ", sorted_rank_array);
+
+    // 順位から元の値をマップさせる
+    let mut sorted_array = (*array).clone();
+    sorted_array.sort();
+
+    // 元の値から順位を対応させるマップ
+    let mut original_to_rank: HashMap<T, usize> = HashMap::new();
+    for i in 0..n {
+        original_to_rank.insert(sorted_array[i], sorted_rank_array[i]);
+    }
+
+    // 元の順序の、順位変換済み配列
+    let mut ranked_array: Vec<usize> = vec![];
+    for i in 0..n {
+        ranked_array.push(*(original_to_rank.get(&array[i]).unwrap()));
+    }
+
+    return (ranked_array, sorted_array, original_to_rank)
 }

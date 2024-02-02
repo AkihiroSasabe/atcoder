614e080fa50b08ab81941349e522b573abe2eaac
AkihiroSasabe <akki.edelweiss@gmail.com>
2024-02-02 23:34:41 +0900

    abc219 c

--- src/abc219/c.rs
+++ src/abc219/c.rs
@@ -14,7 +14,32 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2024-02-02 23:24-23:33 (9min)
     input! {
-        
+        x: Chars,
+        n: usize,
+        s: [Chars; n]
     }
+    let mut hash = HashMap::new();
+    for i in 0..26 {
+        hash.insert(x[i], i);
+    }
+    let mut ss = vec![];
+    for i in 0..n {
+        let mut si = vec![];
+        for j in 0..s[i].len() {
+            si.push(*hash.get(&s[i][j]).unwrap());
+        }
+        ss.push(si);
+    }
+    ss.sort();
+
+    for i in 0..n {
+        for j in 0..ss[i].len() {
+            let num = ss[i][j];
+            print!("{}", x[num]);
+        }
+        println!("");
+    }
+
 }

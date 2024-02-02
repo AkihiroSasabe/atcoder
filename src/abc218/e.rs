2095244374e4ba8f6df6dc6fdf447146d8ab0634
AkihiroSasabe <akki.edelweiss@gmail.com>
2024-02-02 19:57:56 +0900

    218 e

--- src/abc218/e.rs
+++ src/abc218/e.rs
@@ -14,7 +14,111 @@ use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;
 fn main() {
+    // 2024-02-02 19:35-19:50 (15min)
     input! {
-        
+        n: usize,
+        m: usize,
+    }
+    let mut graph = vec![vec![]; n];
+    let mut edges = vec![];
+    let mut uft = UnionFindTree::new(n);
+    let mut reward_sum = 0;
+    for i in 0..m {
+        input!{
+            aii: usize,
+            bii: usize,
+            ci: isize,
+        }
+        let ai = aii - 1;
+        let bi = bii - 1;
+        graph[ai].push((bi, ci));
+        graph[bi].push((ai, ci));
+
+        if ci <= 0 {
+            uft.unite(ai, bi);
+        }
+        else {
+            edges.push((ci as usize, ai, bi));
+            reward_sum += ci as usize;
+        }
+    }
+    edges.sort();
+
+    for i in 0..edges.len() {
+        let ci = edges[i].0;
+        let v0 = edges[i].1;
+        let v1 = edges[i].2;
+        if !uft.issame(v0, v1) {
+            reward_sum -= ci;
+            uft.unite(v0, v1);
+        }
+    }
+    println!("{}", reward_sum);
+
+
+}
+
+
+
+// Union-Find
+// グループの管理（グループ同士の結合や、要素同士の所属か同じか判定）するのに便利なデータ構造
+struct UnionFindTree {
+    parents: Vec<usize>,    // 各頂点の属するグループ(根付き木)の親頂点の番号
+    sizes: Vec<usize>       // 各頂点の属するグループ(根付き木)のサイズ(頂点数)
+}
+
+impl UnionFindTree {
+    // 初期化
+    fn new(n: usize) -> Self {
+        // 各頂点が属するグループの根を格納
+        let parents = (0..n).collect();
+        // 各頂点が属するグループのサイズ(頂点数)を格納。※ただし、更新するのは根のインデックスだけで良い
+        let sizes = vec![1; n];
+        return UnionFindTree {parents, sizes}
+    }
+
+    // 根を求める。経路圧縮により計算量を削減
+    fn root(&mut self, v: usize) -> usize {
+        if self.parents[v] == v {return v}
+        else {
+            // 経路圧縮 (親を根に張り替える。)
+            self.parents[v] = self.root(self.parents[v]);
+            return self.parents[v] as usize
+        }
+    }
+
+    // 同じグループに属するか
+    fn issame(&mut self, v0: usize, v1: usize) -> bool {
+        return self.root(v0) == self.root(v1)
+    }
+
+    // 頂点vが属する根付き木のサイズを取得
+    fn size(&mut self, v: usize) -> usize {
+        let root = self.root(v);
+        return self.sizes[root]
+    }
+
+    // v0を含むグループと、v1を含むグループとを併合する。Union by sizeで計算量削減。
+    fn unite(&mut self, mut v0: usize, mut v1: usize) -> bool {
+        // 既に同じグループであれば何もしない
+        v0 = self.root(v0);
+        v1 = self.root(v1);
+        if v0 == v1 {
+            return false
+        } 
+        let child: usize;
+        let parent: usize;
+        // Union by sizeにより、サイズが小さいグループを、サイズが大きいグループに併合する
+        if self.size(v0) <= self.size(v1) {
+            child = v0;
+            parent = v1;
+        }
+        else {
+            child = v1;
+            parent = v0;
+        }
+        self.sizes[parent] += self.size(child);
+        self.parents[child] = parent;
+        return true
     }
 }

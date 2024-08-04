cde47dc07b616f7f2a57f55f4f794051cea8baee
AkihiroSasabe <akki.edelweiss@gmail.com>
2024-08-04 14:59:29 +0900

    refactor abc365-d

--- src/abc365/d.rs
+++ src/abc365/d.rs
@@ -23,59 +23,30 @@ fn main() {
 
     let mut s = vec![];
     for i in 0..n {
-        let temp = if ss[i] == 'R' {
-            0
-        }
-        else if ss[i] == 'S' {
-            1
-        }
-        else {
-            2
+        let temp = match ss[i] {
+            'R' => 0,
+            'S' => 1,
+            _ => 2,
         };
         s.push(temp);
     }
 
     // dp
     // r: 0, s: 1, p: 2
-    let mut dp = vec![vec![0; 3]; n];
-    if s[0] == 0 {
-        dp[0][2] = 1;
-        dp[0][1] = -1000000000;
-    }
-    else if s[0] == 1{
-        dp[0][0] = 1;
-        dp[0][2] = -1000000000;
-    }
-    else if s[0] == 2 {
-        dp[0][0] = -1000000000;
-        dp[0][1] = 1;
-    }
+    let mut dp = vec![vec![0; 3]; n+1];
+    let NINF = -1_000_000_000;
+    for i in 0..n {
+        let p0 = s[i];              // rock
+        let p1 = (s[i] + 1) % 3;    // scissor
+        let p2 = (s[i] + 2) % 3;    // paper
 
-    for i in 1..n {
-        if s[i] == 0 {
-            dp[i][2] = max(dp[i-1][0] + 1, dp[i-1][1] + 1);
-            dp[i][1] = -1000000000;
-
-            dp[i][0] = max(dp[i-1][1], dp[i-1][2]);
-        }
-        else if s[i] == 1 {
-            dp[i][0] = max(dp[i-1][1] + 1, dp[i-1][2] + 1);            
-            dp[i][2] = -1000000000;
-
-            dp[i][1] = max(dp[i-1][0], dp[i-1][2]);
-        }
-        else if s[i] == 2 {
-            dp[i][0] = -1000000000;
-            dp[i][1] = max(dp[i-1][0] + 1, dp[i-1][2] + 1);
-
-            dp[i][2] = max(dp[i-1][0], dp[i-1][1]);
-        }
+        dp[i+1][p0] = max(dp[i][p1], dp[i][p2]);
+        dp[i+1][p1] = NINF;
+        dp[i+1][p2] = max(dp[i][p0] + 1, dp[i][p1] + 1);
     }
     let mut ans = 0;
     for i in 0..3 {
-        ans = max(ans, dp[n-1][i]);
+        ans = max(ans, dp[n][i]);
     }
     println!("{}", ans);
-
-
 }

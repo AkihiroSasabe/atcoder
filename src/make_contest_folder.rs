e07e95b0c46b4d3aeee31c5333f5218a3fc1b331
AkihiroSasabe <akki.edelweiss@gmail.com>
2025-03-28 19:43:20 +0900

    Add std::cmp::Reverse in make_contest_folder.rs

--- src/make_contest_folder.rs
+++ src/make_contest_folder.rs
@@ -36,17 +36,15 @@ fn main() {
         // proconioを必ず使うので用意
         let content0 = String::from(
 "#![allow(dead_code, unused_imports)]
-use proconio::{input, marker::{Usize1, Isize1}};
+use proconio::{input, marker::{Usize1, Isize1, Chars}};
 use itertools::Itertools;
-use std::cmp::{max, min};
-use std::cmp::Ordering;
+use std::cmp::{max, min, Ordering, Reverse};
 use std::collections::VecDeque;
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::collections::BTreeMap;
 use std::ops::Bound::{Excluded, Included, Unbounded};
 use std::collections::{HashSet, BTreeSet};
-use proconio::marker::Chars;
 use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::*;

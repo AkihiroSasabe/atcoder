2ecc81f8db98c510d6d11679633b64b77b15d3b1
AkihiroSasabe <akki.edelweiss@gmail.com>
2025-10-28 20:10:13 +0900

    Modify template_run.sh to add comments for release-build

--- src/template_run.sh
+++ src/template_run.sh
@@ -11,3 +11,6 @@ CONTEST_NAME=$(echo `pwd` | rev | cut -f -1 -d "/" | rev )
 
 echo $CONTEST_NAME
 cargo run --bin ${CONTEST_NAME}_${1}
+# --release ビルドのとき、proconioは、ターミナルに直接コピペしても入力は受け取れない。
+# テキストファイル (input.txt) などで渡す必要がある。
+# cargo run --release --bin ${CONTEST_NAME}_${1} < input.txt
